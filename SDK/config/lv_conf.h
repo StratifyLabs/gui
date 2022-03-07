cb91c1d20f5404e86f1d153fc5f1272d23284af6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-03-07 09:42:25 -0700

    ModelScope updates

--- SDK/config/lv_conf.h
+++ SDK/config/lv_conf.h
@@ -513,8 +513,16 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_USE_GRID     1
 
 #define LV_USE_PNG 1
-#define LV_USE_GPU_SDL 0
+#define LV_USE_GPU_SDL 1
 #define LV_USE_EXTERNAL_RENDERER LV_USE_GPU_SDL
+
+#if LV_USE_GPU_SDL
+#define LV_GPU_SDL_INCLUDE_PATH <SDL2/SDL.h>
+/*Texture cache size, 8MB by default*/
+#define LV_GPU_SDL_LRU_SIZE (1024 * 1024 * 8)
+/*Custom blend mode for mask drawing, disable if you need to link with older SDL2 lib*/
+#define LV_GPU_SDL_CUSTOM_BLEND_MODE (SDL_VERSION_ATLEAST(2, 0, 6))
+#endif
 
 /*==================
 * EXAMPLES
--- gui/src/model/Model.hpp
+++ gui/src/model/Model.hpp
@@ -5,11 +5,15 @@
 #ifndef GUI_MODEL_HPP
 #define GUI_MODEL_HPP
 
-#include <lvgl/Theme.hpp>
+#include <printer.hpp>
+
 #include <lvgl/Runtime.hpp>
 #include <lvgl/Screen.hpp>
-#include <printer.hpp>
+#include <lvgl/Theme.hpp>
+
+#include <design/ModelScope.hpp>
 #include <design/Worker.hpp>
+
 
 struct Model {
   struct Names {
@@ -36,11 +40,11 @@ struct Model {
   lvgl::Screen about_screen = lvgl::Screen(Names::about_screen_name);
   lvgl::Screen github_screen = lvgl::Screen(Names::github_screen_name);
   lvgl::Screen files_screen = lvgl::Screen(Names::files_screen_name);
-  lvgl::Screen home_screen = lvgl::Screen((lv_obj_t*)nullptr);
+  lvgl::Screen home_screen = lvgl::Screen((lv_obj_t *)nullptr);
 
   // this can be used to create a thread to process
   // background tasks
-  lvgl::Runtime * runtime = nullptr;
+  lvgl::Runtime *runtime = nullptr;
 
   // We will use this to print debug messages on the Terminal
   printer::YamlPrinter printer;
@@ -61,9 +65,8 @@ private:
   design::ModelScope::Construct model_scope_options;
 };
 
-class ModelAccess : public api::ExecutionContext{
+class ModelAccess : public api::ExecutionContext {
 public:
-
   static Model &model() {
     // make sure the caller has locked the model
     Model &result = Model::instance();
@@ -71,10 +74,7 @@ public:
     return Model::instance();
   }
 
-  static Printer & printer(){
-    return model().printer;
-  }
-
+  static Printer &printer() { return model().printer; }
 };
 
 #endif // GUI_MODEL_HPP
